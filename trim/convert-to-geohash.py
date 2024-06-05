c5a3d50c2980290b3f9d6adb34ef8010c094fd59
Andy Chuang <andy.chuang@gogoro.com>
2024-06-05 11:19:01 +0800

    feat: add geohash sample

---
+++ trim/convert-to-geohash.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import geohash
+import os
+
+# 設定基準路徑為專案的根目錄
+basePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 結合基準路徑和相對路徑來獲取絕對路徑
+
+
+def get_absolute_path(relative_path):
+    return os.path.abspath(os.path.join(basePath, relative_path))
+
+
+filePath = get_absolute_path('init-data/northern-mrt.csv')
+print(filePath)
+print(basePath)
+
+# 讀取 CSV 文件
+df = pd.read_csv(filePath)
+
+# 定義一個函數將經緯度轉換為 geohash
+
+
+def latlon_to_geohash(row):
+    return geohash.encode(row['lat'], row['lon'])
+
+
+# 應用函數到 DataFrame 的每一行，並新增一個 geohash 欄位
+df['geohash'] = df.apply(latlon_to_geohash, axis=1)
+
+# 將結果寫回到新的 CSV 文件
+df.to_csv(basePath+'/geohash-northern-mrt.csv', index=False)
--- trim/merge.py
+++ trim/merge.py
@@ -30,6 +30,8 @@ merged_df = pd.concat([organize_df, address_df['里'],
                       address_df['Response_X'], address_df['Response_Y']], axis=1)
 merged_df.rename(columns={'Response_X': '經度',
                  'Response_Y': '緯度'}, inplace=True)
+
+
 merged_df.to_csv(basePath+'/台北市-112-112-merged.csv')
 
 # TODO: 捷運座標 to geoHash
