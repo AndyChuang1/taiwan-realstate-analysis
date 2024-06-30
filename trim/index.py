8ed5381594717a353856a0ca1199305bf8870e91
andychuang1 <bboy81905@gmail.com>
2024-06-30 14:54:08 +0800

    refactor: to new taipei city

--- trim/index.py
+++ trim/index.py
@@ -8,7 +8,7 @@ from matplotlib.font_manager import FontProperties
 parser = argparse.ArgumentParser(
     description='Choose the location you want to analyze')
 parser.add_argument(
-    '-c', '--city', help="['臺北市','苗栗縣','花蓮縣','臺中市','臺中縣','臺東縣','基隆市','南投縣','澎湖縣','臺南市','彰化縣','陽明山','高雄市','雲林縣','金門縣','臺北縣','嘉義縣','連江縣','宜蘭縣','臺南縣','嘉義市','桃園縣','高雄縣','新竹市','新竹縣','屏東縣']", default='臺北市')
+    '-c', '--city', help="['臺北市','苗栗縣','花蓮縣','臺中市','臺中縣','臺東縣','基隆市','南投縣','澎湖縣','臺南市','彰化縣','陽明山','高雄市','雲林縣','金門縣','新北市','嘉義縣','連江縣','宜蘭縣','臺南縣','嘉義市','桃園縣','高雄縣','新竹市','新竹縣','屏東縣']", default='臺北市')
 parser.add_argument(
     '-f', '--yearsfrom', help='from year, example"110', type=int, default=112)
 parser.add_argument(
